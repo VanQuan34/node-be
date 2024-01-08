791618acccb8edaad1657e1593c1406404e8c5e2
quantv <tavananhquan96@gmail.com>
2024-01-08 22:57:35 +0700

    fix jwt response

--- _middleware/authorize.js
+++ _middleware/authorize.js
@@ -12,7 +12,7 @@ function authorize() {
         // attach full user record to request object
         async (req, res, next) => {
             // get user with id from token 'sub' (subject) property
-            const user = await db.User.findByPk(req.user.sub);
+            const user = await db.User.findByPk(req.user.sub.id);
 
             // check user still exists
             if (!user)
