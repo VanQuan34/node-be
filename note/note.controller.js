be3bcbe30142dc4f9f08a09f921b06b7d7eeb62e
quantv <quantv@mobio.io>
2024-01-24 17:01:37 +0700

    edit note api

--- note/note.controller.js
+++ note/note.controller.js
@@ -10,8 +10,8 @@ router.post('/create', authorize(), createSchema, create);
 router.get('/', authorize(), getAll);
 router.get('/current', authorize(), getCurrent);
 router.get('/details/:note_id', authorize(), getDetailById);
-router.get('/category/:category_id', authorize(), getById);
-router.put('/:id', authorize(), updateSchema, update);
+router.get('/category/:category_id', authorize(), getNoteByCategory);
+router.patch('/:id', authorize(), updateSchema, update);
 router.delete('/:id', authorize(), _delete);
 
 module.exports = router;
@@ -63,9 +63,9 @@ function getCurrent(req, res, next) {
     res.json(req.user);
 }
 
-function getById(req, res, next) {
+function getNoteByCategory(req, res, next) {
   console.log('req.params.category_id=', req.params.category_id);
-    noteService.getById(req.params.category_id)
+    noteService.getNoteByCategory(req.params.category_id)
         .then(note => res.json(note))
         .catch(next);
 }
@@ -78,19 +78,16 @@ function getDetailById(req, res, next){
 
 function updateSchema(req, res, next) {
     const schema = Joi.object({
-        firstName: Joi.string().empty(''),
-        lastName: Joi.string().empty(''),
-        username: Joi.string().empty(''),
-        password: Joi.string().min(6).empty('')
+        content: Joi.string(),
     });
     validateRequest(req, next, schema);
 }
 
 function update(req, res, next) {
-    noteService.update(req.params.id, req.body)
-        .then(user => res.json({
+    noteService.update(req.params.id, req.body.content)
+        .then(note => res.json({
                 code: 200,
-                data: user,
+                data: note,
                 message: 'Update successful'
             })
             )
@@ -99,6 +96,11 @@ function update(req, res, next) {
 
 function _delete(req, res, next) {
     noteService.delete(req.params.id)
-        .then(() => res.json({ message: 'User deleted successfully' }))
+        .then(() => res.json(
+            {
+                code: 200,
+                message: 'Note deleted successfully'
+            })
+        )
         .catch(next);
 }
--- note/note.service.js
+++ note/note.service.js
@@ -8,6 +8,7 @@ module.exports = {
     authenticate,
     getAll,
     getById,
+    getNoteByCategory,
     getDetailsById,
     create,
     update,
@@ -56,42 +57,47 @@ async function getDetailsById(id) {
     return await getDetailsNote(id);
 }
 
+async function getNoteByCategory(id){
+    const note = await db.Note.findAll({ where: { category_id: id } });
+    if (!note){
+      return {
+        code : 200,
+        data: [],
+        message: 'Request success'
+      }
+    }
+    return {
+      code : 200,
+      data: note,
+      message: 'Request success'
+    }
+}
+
 async function create(params) {
     await db.Note.create(params);
     return params;
 }
 
-async function update(id, params) {
-    const user = await getNote(id);
-
-    // validate
-    const usernameChanged = params.username && user.username !== params.username;
-    if (usernameChanged && await db.User.findOne({ where: { username: params.username } })) {
-        throw 'Username "' + params.username + '" is already taken';
-    }
-
-    // hash password if it was entered
-    if (params.password) {
-        params.hash = await bcrypt.hash(params.password, 10);
-    }
-
-    // copy params to user and save
-    Object.assign(user, params);
-    await user.save();
-
-    return omitHash(user.get());
+async function update(id, content) {
+    const note = await getNote(id);
+    if(!note) throw 'Invalid note';
+    note.content = content;
+    await note.save();
+    return note.get();
 }
 
 async function _delete(id) {
-    const user = await getNote(id);
-    await user.destroy();
+    const note = await getNote(id);
+    await note.destroy();
 }
 
 // helper functions
 
 async function getNote(id) {
     // const note = await db.User.findByPk(id);
-    const note = await db.Note.findAll({ where: { category_id: id } });
+    const note = await db.Note.findOne({ where: { note_id: id } });
+    if (!note) throw 'Note not found';
+    return note;
     if (!note){
       return {
         code : 200,
